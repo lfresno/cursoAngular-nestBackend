7503a544628bdefb962b7688128ccd5786c67dc9
Lucia Fresno <lucia.fresno.olmeda@gmail.com>
2024-01-02 11:11:20 +0100

    Preparación para desplegar el backend en Railway con MongoDB

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   //sirve para restringir el backend. Si no recibe la info como la quiere, se rechaza.
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +16,6 @@ async function bootstrap() {
    );
 
    
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
